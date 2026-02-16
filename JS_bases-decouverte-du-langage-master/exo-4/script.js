06ba63d6e4962d25579648d07b76fef4713a9c78
rdesfonds-campus <romain.desfonds@gmail.com>
2026-02-16 11:56:56 +0100

    exercice 4

--- JS_bases-decouverte-du-langage-master/exo-4/script.js
+++
@@ -1,5 +0,0 @@
-// you can write js here
-
-console.log('exo-4');
-
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
